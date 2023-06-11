b9268ee9d91813102375d9753e58667c8fedb93c
Neal <nealforester@gmail.com>
2023-06-11 07:16:26 -0400

    Create deleteSingle.py

---
+++ deleteSingle.py
@@ -0,0 +1,56 @@
+from datetime import datetime,timedelta,date,time
+import time
+import sqlite3
+
+def displaySingleDate():
+    # Connect to the SQLite database file
+    conn = sqlite3.connect('/home/neal/oopTemp/oopTemp.db')
+
+    # Create a cursor object to execute SQL queries
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM data WHERE strftime('%d',date) = ?",[dateValue])
+    
+    rows = cursor.fetchall()
+
+    # Iterate over the rows and print the data
+    for row in rows:
+       id, date, piTemp, piHumidity, webTemp, webOvercast = row
+       print(f"ID: {id}, Date: {date}, Temperature: {piTemp}, Humidity: {piHumidity}, Web Temp: {webTemp}, Cloud Info: {webOvercast}")
+
+def deleteSingleItem():
+    # Connect to the SQLite database file
+    conn = sqlite3.connect('/home/neal/oopTemp/oopTemp.db')
+
+    # Create a cursor object to execute SQL queries
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM data WHERE strftime('%d',date) = ? and ID = ?",[dateValue,value])
+    
+    rows = cursor.fetchall()
+
+    # Iterate over the rows and print the data
+    for row in rows:
+       id, date, piTemp, piHumidity, webTemp, webOvercast = row
+       print(f"ID: {id}, Date: {date}, Temperature: {piTemp}, Humidity: {piHumidity}, Web Temp: {webTemp}, Cloud Info: {webOvercast}")
+    
+    cursor.execute("DELETE FROM data WHERE strftime('%d',date) = ? and ID = ?",[dateValue,value])
+    
+    #Commit your changes in the database
+    conn.commit()
+    
+    # Close the cursor and connection
+    cursor.close()
+    conn.close()
+
+dateValue = input("Enter date which contains the item you wish to remove DD):\n")
+if (len(dateValue) == 1):
+    dateValue = '0' + dateValue
+
+print('Searching for\n\n',dateValue)    
+displaySingleDate()
+
+value = input("Enter item number to remove:\n\n")
+deleteSingleItem()
+
+print('Item Deleted')
