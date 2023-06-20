a27b23095174690937dc35d5a22543b523d92cf8
Neal <nealforester@gmail.com>
2023-06-20 01:31:09 -0400

    Update errorHandling.py
    filename tweak

--- errorHandling.py
+++ errorHandling.py
@@ -1,5 +1,7 @@
 import logging
+from datetime import datetime
 
 def sendError():
-   logging.basicConfig(filename='errorTemp.log')
-   logging.warning('Reading Weather API Failed after 6 attempts')
+   logging.basicConfig(format='%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+                       datefmt='%Y-%m-%d:%H:%M:%S',level=logging.DEBUG, filename='{:%Y-%m-%d}.log'.format(datetime.now()))
+   logging.debug('Reading Weather API Failed after 6 attempts')
