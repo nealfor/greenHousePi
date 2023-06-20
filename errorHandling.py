8730ea3f20323b7eba27934506cf7d60f2880b97
Neal <nealforester@gmail.com>
2023-06-20 01:50:55 -0400

    Update errorHandling.py
    log the passed string for more reusability

--- errorHandling.py
+++ errorHandling.py
@@ -1,7 +1,7 @@
 import logging
 from datetime import datetime
 
-def sendError():
+def sendError(errText):
    logging.basicConfig(format='%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
                        datefmt='%Y-%m-%d:%H:%M:%S',level=logging.DEBUG, filename='{:%Y-%m-%d}.log'.format(datetime.now()))
-   logging.debug('Reading Weather API Failed after 6 attempts')
+   logging.debug(errText)
